8e867fde1fb502806264167fb1bc52b686f7298a
Rashgild <Rashgild@gmail.com>
2018-04-12 08:47:46 +0400

    -fix:reason of disability

--- src/ru/my/helpers_operations/StoredQuery.java
+++ src/ru/my/helpers_operations/StoredQuery.java
@@ -47,7 +47,7 @@ public class StoredQuery {
                 ",case when dd.anotherlpu_id is not null then dd.anotherlpuogrn else ''||lpu.ogrn end as LPU_OGRN\n" +
                 ",p.birthday as BIRTHDAY\n" +
                 ",case when sex.omccode = '1' then '0' else '1' end as GENDER\n" +
-                ",vdr.codef as REASON1\n" +
+                ",vdr.code as REASON1\n" +
                 ",vdr2.code as REASON2\n" +
                 ",vdr3.code as REASON3\n" +
                 ",mkb.code as DIAGNOS\n" +
